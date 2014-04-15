15273be578afe8f831cb309795fce03a132ba1e2
peter tinti & anne jhetson <student@epicodus.com>
2014-04-15 10:32:09 -0700

    initial commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ActionMailer::Base
+  default from: "letsgoharper@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome(user)
+    @user = user
+    @greeting = "Hi"
+    @name = @user.email[0..(@user.email.index('@')-1)]
+
+    mail to: user.email, subject: "Welcome to Tweetr"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Tweetr::Application.routes.draw do
+  devise_for :users
+  root "users#index"
+end
+
